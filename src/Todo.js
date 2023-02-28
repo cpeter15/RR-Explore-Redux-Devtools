df7cdcfb50eb1f76288ec7e63b3bf83b2273811d
cpeter15 <caleb.j.peterson9687@gmail.com>
2023-02-28 13:55:01 -0500

    added todo ui

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { addTodo, removeOne, clearTodo, removeTodo } from './features/todoSlice'
+import { addTodo, clearTodo, removeTodo } from './features/todoSlice'
 
 function Todo() {
     const items = useSelector((state) => state.todos.items)
